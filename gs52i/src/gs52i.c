ff26d7c5022ed7ae968df96812bb66e44168e603
Karl Zander <iagmla@nifu.local>
2024-05-05 08:56:22 -0700

    gs52i

---
+++ gs52i/src/gs52i.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h> 
+#include <stdint.h>
+#include "geistshifter52.c"
+
+/* Das GeistShifter 52 */
+/* by Karl Zander */
+
+void usage() {
+    printf("Das GeistShifter 52 v0.0.1\n");
+    printf("by KryptoMagick\n\n");
+    printf("Usage: gs52 <-e/-d> <input file> <output file> <key length 52>\n");
+}
+
+int main(int argc, char *argv[]) {
+    char *encryptSymbol = "-e";
+    char *decryptSymbol = "-d";
+    char *inFilename, *outFilename;
+    uint8_t *key;
+    char *mode = argv[1];
+    inFilename = argv[2];
+    outFilename = argv[3];
+    key = argv[4];
+
+    if (argc != 5) {
+        usage();
+        return 0;
+    }
+
+    if (strcmp(mode, encryptSymbol) == 0) {
+        gs52encrypt(inFilename, outFilename, key);
+    }
+    else if (strcmp(mode, decryptSymbol) == 0) {
+        gs52decrypt(inFilename, outFilename, key);
+    }
+    return 0;
+}
